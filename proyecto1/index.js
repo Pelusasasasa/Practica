e616b54593aac52268dfce957c1cead0de5f9eb9
Pelusa <agustinlorenzatto@gmail.com>
2023-03-21 17:57:50 -0300

    puse mi portafolio en el proyecto y tambien un jeugo de quiz

---
+++ proyecto1/index.js
@@ -0,0 +1,16 @@
+const corregir = document.getElementById('corregir');
+const cantidad = document.getElementById('cantidad');
+
+const inputs = document.querySelectorAll('input[type=radio]');
+
+let contador = 0;
+
+corregir.addEventListener('click',e=>{
+    for(let input of inputs){
+        if (input.checked && input.value === "true") {
+            contador++;
+        }
+    }
+
+    cantidad.innerHTML = contador;
+});
