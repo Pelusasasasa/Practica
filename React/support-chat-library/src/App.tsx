8fdf7cfaf2ebef4bb39e18b191272f9cf819364b
Pelusa <agustinlorenzatto@outlook.com>
2025-10-10 23:52:57 -0300

    Sioguiendo el curso

--- React/support-chat-library/src/App.tsx
+++ React/support-chat-library/src/App.tsx
@@ -1,11 +1,17 @@
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRouter } from './AppRouter'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
      <AppRouter />
-    </>
+     <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
--- React/support-chat-library/src/chat/layout/ChatLayout.tsx
+++ React/support-chat-library/src/chat/layout/ChatLayout.tsx
@@ -1,8 +1,7 @@
 
 import { Button } from "@/components/ui/button"
 import { X } from "lucide-react"
-import { Outlet } from "react-router"
-import { ContactInfo } from "../components/ContactInfo"
+import { Link, Outlet } from "react-router"
 import { ContactList } from "../components/ContactList"
 import { NoContactSelected } from "../components/NoContactSelected"
 import { ContactInfoSkeleton } from "../components/ContactInfoSkeleton"
@@ -15,7 +14,9 @@ export default function ChatLayout() {
                 <div className="p-4 border-b">
                     <div className="flex items-center gap-2">
                         <div className="h-6 w-6 rounded-full bg-primary" />
-                        <span className="font-semibold">NexTalk</span>
+                        <Link to='/chat'>
+                            <span className="font-semibold">NexTalk</span>
+                        </Link>
                     </div>
                 </div>
                 <ContactList/>
